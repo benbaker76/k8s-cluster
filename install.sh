de138b1de68842e2539b6236d2e310de459b57ae
headkaze <headkaze@gmail.com>
2022-11-28 14:51:22 -0500

    move globals into hosts.ini

--- install.sh
+++ install.sh
@@ -4,4 +4,5 @@ ansible-playbook -i hosts.ini ./init.yml
 ansible-playbook -i hosts.ini ./kube-dependencies.yml
 ansible-playbook -i hosts.ini ./control-planes.yml
 ansible-playbook -i hosts.ini ./workers.yml
+ansible-playbook -i hosts.ini ./install-tools.yml
 
