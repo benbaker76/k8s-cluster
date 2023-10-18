ca3e09b54efde50527f69714163401fe64737faf
benbaker76 <headkaze@gmail.com>
2023-10-17 17:02:42 -0700

    Add support for cilium

--- create-config.sh
+++ create-config.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null ubuntu@192.168.122.191 mkdir /home/ubuntu/.kube
-ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null ubuntu@192.168.122.191 sudo cp /etc/kubernetes/admin.conf /home/ubuntu/.kube/config
-ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null ubuntu@192.168.122.191 sudo chmod +r /home/ubuntu/.kube/config
+#ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null ubuntu@192.168.122.191 mkdir /home/ubuntu/.kube
+#ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null ubuntu@192.168.122.191 sudo cp /etc/kubernetes/admin.conf /home/ubuntu/.kube/config
+#ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null ubuntu@192.168.122.191 sudo chmod +r /home/ubuntu/.kube/config
 scp -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null ubuntu@192.168.122.191:/home/ubuntu/.kube/config ~/.kube
